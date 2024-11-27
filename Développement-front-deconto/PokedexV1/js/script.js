2e17b5f17f17ba9dd52bd4e3995b8c4321704d15
eliott-colin <eliott.colin.pro@gmail.com>
2024-11-27 13:37:17 +0100

    Double type

--- Développement-front-deconto/PokedexV1/js/script.js
+++ Développement-front-deconto/PokedexV1/js/script.js
@@ -1,6 +1,6 @@
 async function getData(...params) {
     //récup données
-    const data = await fetch("./data/data.json")
+    const data = await fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/1")
     .then(response => response.json())
     .catch(error => alert("Erreur : " + error));
     console.log(data);
@@ -14,7 +14,10 @@ async function getData(...params) {
             <img src="${element.image}"/>
           </picture>
           <figcaption>
-            <span class="types">${element.apiTypes[0].name}</span>
+            <div class="containerTypes">
+            <span class="types"><img src="${element.apiTypes[1] ? element.apiTypes[1].image: element.apiTypes[0].image}" style="width:25px;">${element.apiTypes[1] ? element.apiTypes[1].name: element.apiTypes[0].name}${cardColor(element,article)}</span>
+            ${doubleType(element)}
+            </div>
             <h2>${element.name}</h2>
             <ol>
               <li>Points de vie : ${element.stats.HP} </li>
@@ -25,10 +28,97 @@ async function getData(...params) {
             </ol>
           </figcaption>
         </figure>`
-      
+ 
       document.querySelector("main").append(article)
         console.log(element)
     });
+
+      //Changer couleur carte
+      function doubleType(element) {
+        if (element.apiTypes[1] != null) {
+            return `<span class="types2"><img src="${element.apiTypes[0].image}" style="width:25px;"> ${element.apiTypes[0].name}</span>`
+        }
+        return ""
+      }
+
+      //Changer couleur carte
+      function cardColor(element,article) {
+        switch (element.apiTypes[1] ? element.apiTypes[1].name: element.apiTypes[0].name){
+            case 'Plante' :
+                article.style.backgroundColor = "darkgreen";
+                article.style.borderColor = "darkgreen";
+                break;
+            case 'Eau' :
+                article.style.backgroundColor = "blue";
+                article.style.borderColor = "blue";
+                break;
+            case 'Feu' :
+                article.style.backgroundColor = "red";
+                article.style.borderColor = "red";
+                break;
+            case 'Combat' :
+                article.style.backgroundColor = "orange";
+                article.style.borderColor = "orange";
+                break;
+            case 'Vol' :
+                article.style.backgroundColor = "lightblue";
+                article.style.borderColor = "lightblue";
+                break;
+            case 'Électrik' :
+                article.style.backgroundColor = "yellow";
+                article.style.borderColor = "yellow";
+                break;
+            case 'Sol' :
+                article.style.backgroundColor = "#8b4513";
+                article.style.borderColor = "#8b4513";
+                break;
+            case 'Roche' :
+                article.style.backgroundColor = "darkgoldenrod";
+                article.style.borderColor = "darkgoldenrod";
+                break;
+            case 'Psy' :
+                article.style.backgroundColor = "purple";
+                article.style.borderColor = "purple";
+                break;
+            case 'Ténèbres' :
+                article.style.backgroundColor = "black";
+                article.style.borderColor = "black";
+                break;
+            case 'Spectre' :
+                article.style.backgroundColor = "indigo";
+                article.style.borderColor = "indigo";
+                break;
+            case 'Dragon' :
+                article.style.backgroundColor = "darkblue";
+                article.style.borderColor = "darkblue";
+                break;
+            case 'Fée' :
+                article.style.backgroundColor = "#FFC0CB";
+                article.style.borderColor = "#FFC0CB";
+                break;
+            case 'Acier' :
+                article.style.backgroundColor = "gray";
+                article.style.borderColor = "gray";
+                break;
+            case 'Poison' :
+                article.style.backgroundColor = "darkviolet";
+                article.style.borderColor = "darkviolet";
+                break;
+            case 'Insecte' :
+                article.style.backgroundColor = "lightgreen";
+                article.style.borderColor = "lightgreen";
+                break;
+            case 'Glace' :
+                article.style.backgroundColor = "cyan";
+                article.style.borderColor = "cyan";
+                break;
+            case 'Normal' :
+                article.style.backgroundColor = "beige";
+                article.style.borderColor = "beige";
+                break;  
+        }
+        return ""
+    }
 }
 
 getData();
